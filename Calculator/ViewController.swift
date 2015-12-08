7f868719652fe9a7e15d1ddcbfa35a7d3ca47f16
Wallace Wu <cwwwu@uwaterloo.ca>
2015-12-08 00:35:31 -0500

    Project 1 - Required Task: extend calculator with additional functionality - Record history of operands and operations - Add 'clear' button - Add additional operations: sin, cos - Add button to enter 'pi' constant - Add ability to enter floating number and prevent user from entering invalid floating number

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -11,16 +11,45 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var display: UILabel!
+    @IBOutlet weak var history: UILabel!
     
     var userIsTheMiddleOfTypingANumber = false
+    
+    @IBAction func clearOperandStack() {
+        userIsTheMiddleOfTypingANumber = false
+        displayValue = 0
+        operandStack.removeAll()
+        history.text = " "
+    }
     
     @IBAction func appendDigit(sender: UIButton) {
         let digit = sender.currentTitle!
         if userIsTheMiddleOfTypingANumber {
-            display.text = display.text! + digit
+            if digit != "." || display.text!.rangeOfString(".") == nil {
+                display.text = display.text! + digit
+            }
         } else {
             display.text = digit
             userIsTheMiddleOfTypingANumber = true
+        }
+    }
+    
+    @IBAction func enterConstant(sender: UIButton) {
+        if userIsTheMiddleOfTypingANumber {
+            enter()
+        }
+        
+        var validConstant = true
+        let constant = sender.currentTitle!
+        switch constant {
+        case "π":
+            displayValue = M_PI
+            enter()
+        default: validConstant = false
+        }
+        
+        if validConstant {
+            history.text = history.text! + " \(constant)"
         }
     }
     
@@ -31,13 +60,21 @@ class ViewController: UIViewController {
             enter()
         }
         
+        var validOperation = true
+        
         switch operation {
         case "×": performOperation { $0 * $1 }
         case "÷": performOperation { $1 / $0 }
         case "+": performOperation { $0 + $1 }
         case "−": performOperation { $1 - $0 }
         case "√": performOperation { sqrt($0) }
-        default: break
+        case "sin": performOperation { sin($0) }
+        case "cos": performOperation { cos($0) }
+        default: validOperation = false
+        }
+        
+        if validOperation {
+            history.text = history.text! + " \(operation)"
         }
     }
     
@@ -60,6 +97,10 @@ class ViewController: UIViewController {
     var operandStack = Array<Double>()
     
     @IBAction func enter() {
+        if userIsTheMiddleOfTypingANumber {
+            history.text = history.text! + " " + display.text!
+        }
+        
         userIsTheMiddleOfTypingANumber = false
         operandStack.append(displayValue)
         print("operandStack = \(operandStack)")
